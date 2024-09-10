2ac6621a9109f3ab85b0dc21386a14d288b25d60
Marina <climovicimarina@gmail.com>
2024-09-10 17:33:32 +0300

    templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 
 def hello(request):
-    template = loader.get_template('hello.html')
-    return HttpResponse(template.render())
+    users = User.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+        'users': users,
+    }
+    return HttpResponse(template.render(context, request))
