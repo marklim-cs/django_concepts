9c30d2c981910f26e6b20f87403adb94e6521d1c
Marina <climovicimarina@gmail.com>
2024-09-11 14:28:39 +0300

    add link to details

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
-    path('hello/', views.hello, name='hello')
+    path('', views.all_users, name='all_users'),
+    path('users/', views.all_users, name='all_users'),
+    path('users/details/<int:id>', views.details, name='details')
 ]
--- app/views.py
+++ app/views.py
@@ -2,10 +2,18 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import User
 
-def hello(request):
+def all_users(request):
     users = User.objects.all().values()
     template = loader.get_template('all_users.html')
     context = {
         'users': users,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    user = User.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'user': user,
     }
     return HttpResponse(template.render(context, request))
